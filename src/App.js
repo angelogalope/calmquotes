1006d090c961a1e3652923e23d9d060cdafe9d1d
Angelo Galope <105281585+angelogalope@users.noreply.github.com>
2024-02-09 12:31:02 +0800

    Added animation(unfinished)

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
+import { Analytics } from '@vercel/analytics/react';
 import { useState } from 'react';
+import 'animate.css';
 import './App.css';
 
 function App() {
-  const [quote, setQuote] = useState(null)
+  const [quote, setQuote] = useState(null);
+  const [buttonClicked, setButtonClicked] = useState(false);
+
   const categories = [
     "age", "alone", "anger", "attitude", "beauty", "courage", 
     "friendship", "education", "failure", "family", "fear",
     "forgiveness", "future", "happiness", "jealousy", "morning", "success"
-];
+  ];
 
-const randomIndex = Math.floor(Math.random() * categories.length);
-const randomCategory = categories[randomIndex];
+  const randomIndex = Math.floor(Math.random() * categories.length);
+  const randomCategory = categories[randomIndex];
 
   const getQuote = async () => {
     try {
-        const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=' + randomCategory, {
-            headers: {
-                'X-Api-Key': 'wvuJDEi3dEI1saJgCzMcjQ==zswnxcksGTi7ELsN'
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error('Failed to fetch data');
-        }
-
-        const result = await response.json();
-        setQuote(result[0]);
+      setQuote(null);
+      setButtonClicked(true);
+  
+      const response = await fetch('https://api.quotable.io/random');
+  
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+  
+      const result = await response.json();
+      setQuote(result);
     } catch (error) {
-        console.error('Error fetching data:', error);
-        alert("Error fetching data");
+      console.error('Error fetching data:', error);
+      alert("Error fetching data");
     }
   };
+  
 
   return (
     <div className="App flex flex-col items-center gap-24 p-10 w-full h-screen bg-gradient-to-tr from-primColor to-secColor">
@@ -43,15 +47,25 @@ const randomCategory = categories[randomIndex];
         </p>
       </div>
       <div className='text-white flex flex-col items-center text-center w-auto px-10'>
-        <h1 className='courgette-regular text-3xl md:text-6xl'>"</h1>
         {quote && (
-          <div>
+          <div className='quote-container animate__animated animate__fadeIn'>
+            <h1 className='courgette-regular text-3xl md:text-6xl'>"</h1>
             <p className='courgette-regular text-xl md:text-4xl'>
-              {quote.quote}
+              {quote.content}
             </p>
             <p className='courgette-regular text-md md:text-2xl'>-{quote.author}-</p>
           </div>
         )}
+       {!quote && (
+          buttonClicked && (
+            <div>
+              <p className='courgette-regular text-xl md:text-4xl'>
+                -Click the button to show a quote-
+              </p>
+            </div>
+          )
+        )}
+
         <button type='submit' onClick={getQuote} className='flex items-center justify-center mt-10 px-4 py-1 rounded-md md:rounded-lg bg-white text-secColor font-semibold hover:bg-gray-200 duration-500'>
           Show a quote
         </button>
@@ -59,6 +73,7 @@ const randomCategory = categories[randomIndex];
       <div className='fixed bottom-6'>
         <h1 className='text-gray-200 text-[8px] md:text-sm'>Made with ❤️ by Angelo Galope</h1>
       </div>
+      <Analytics />
     </div>
   );
 }
